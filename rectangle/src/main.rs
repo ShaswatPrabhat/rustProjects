803070d5691cc1c4e2adee50ac78766ce0f6a67d
Shaswat Prabhat <spd1238@gmail.com>
2020-05-09 09:01:57 +0530

    Rectange struct with associated functions and methods.

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -1,15 +1,38 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        println!("Rectangle info {:#?}", self);
+        self.height * self.width
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 fn main() {
     let rect1 = Rectangle {
         height: 23,
         width: 31,
     };
-    println!("Area of rectangle is {}", area(&rect1));
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let square1 = Rectangle::square(31);
+    println!(
+        "Area of rectangle with width {} and height {} is {}",
+        rect1.width,
+        rect1.height,
+        rect1.area()
+    );
+    println!(
+        "Area of rectangle with width {} and height {} is {}",
+        square1.width,
+        square1.height,
+        square1.area()
+    );
 }
